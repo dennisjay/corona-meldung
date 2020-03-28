f11bfdc32829c3724561b344c617f58e7b67b8e2
tbmgrtnr <51128391+tbmgrtnr@users.noreply.github.com>
2020-03-28 20:16:39 +0100

    changed error text and added 'see also spam folder'

--- src/containers/Fragebogen/index.js
+++ src/containers/Fragebogen/index.js
@@ -136,7 +136,7 @@ class Fragebogen extends React.Component {
               this.handleNext();
             })
             .catch( () => {
-              window.confirm("Bitte gib eine gültige Mail-Adresse ein.")
+              window.confirm("Bitte gib eine gültige Mail-Adresse ein. Jede Mail-Adresse kann zudem nur einmal verwendet werden.")
             })
         }
         break;
@@ -151,7 +151,7 @@ class Fragebogen extends React.Component {
             this.handleNext();
           })
           .catch( () => {
-            window.confirm("Bitte gib einen gültigen code ein")
+            window.confirm("Das ist nicht der richtige Code.")
           });
           break;
 
@@ -239,7 +239,8 @@ class Fragebogen extends React.Component {
                     <Box style={{margin: "auto"}}>
                         <Typography variant="h5" color="primary" >Schau in deine Mails</Typography><br />
                         <Typography> und gib den <b>Code</b> ein, den wir dir geschickt haben:</Typography><br />
-                        <TextField variant="outlined" label="Code" style={{minWidth: 300}} onChange={event=> { this.setState({code: event.target.value}) }} onKeyDown={key=>{ if (key.keyCode===13) { this.handleWeiter() } }} />
+                        <TextField variant="outlined" label="Code" style={{minWidth: 300}} onChange={event=> { this.setState({code: event.target.value}) }} onKeyDown={key=>{ if (key.keyCode===13) { this.handleWeiter() } }} /><br />
+                        <Typography variant="caption" style={{marginLeft: 15, marginTop: 10, color: "#bdbdbd"}}>Schau ggf. in deinen <b>Spam-Ordner</b>.</Typography><br />
                     </Box>
                   </Grid>
 
