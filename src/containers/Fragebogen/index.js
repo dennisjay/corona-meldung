2959856188e9aac8820f9d2a11b604cc42a2d37d
Dennis Jöst <djprivat@googlemail.com>
2020-03-30 18:19:29 +0200

    Warn the user before leaving the form

--- src/containers/Fragebogen/index.js
+++ src/containers/Fragebogen/index.js
@@ -148,6 +148,16 @@ class Fragebogen extends React.Component {
         this.state = this.defaultState
     }
 
+
+  componentDidMount() {
+    window.addEventListener('beforeunload', function (e) {
+      // Cancel the event
+      e.preventDefault(); // If you prevent default behavior in Mozilla Firefox prompt will always be shown
+      // Chrome requires returnValue to be set
+      e.returnValue = '';
+    });
+  }
+
   handleNext = () => {
     this.setState(state => ({
         activeStep: state.activeStep + 1,
